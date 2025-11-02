f7419a758b1d87435905f750c7099013fbf67235
Bindusgit <bindudwork@gmail.com>
2025-11-02 17:00:47 +0530

    Make create place you orderin checkout, and add to cart in orders page to work

--- src/pages/checkout/PaymentSummary.jsx
+++ src/pages/checkout/PaymentSummary.jsx
@@ -1,5 +1,17 @@
+import { useNavigate } from 'react-router'
 import { formatMoney } from '../../utils/money'
-export function PaymentSummary({ paymentSummary }) {
+import axios from 'axios'
+
+export function PaymentSummary({ paymentSummary, loadCart }) {
+  const navigate = useNavigate()
+  const createOrder = async () => {
+      await axios.post('/api/orders')
+      await loadCart()
+      navigate('/orders')
+  }
+  
+  
+  
   return (
     <div className="payment-summary">
       <div className="payment-summary-title">
@@ -33,7 +45,7 @@ export function PaymentSummary({ paymentSummary }) {
             <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostCents)}</div>
           </div>
 
-          <button className="place-order-button button-primary">
+          <button className="place-order-button button-primary" onClick={createOrder}>
             Place your order
           </button>
         </>
