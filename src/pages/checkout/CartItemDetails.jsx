b4ee2fcf181aafa87d6ea983d806d18c23344ffa
Bindusgit <bindudwork@gmail.com>
2025-11-02 14:17:22 +0530

    create update, delete  items feature in cart

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,25 +1,42 @@
 import { formatMoney } from '../../utils/money'
+import axios from 'axios'
+import { useState } from 'react'
 
-function CartItemDetails({ cartItem }) {
+function CartItemDetails({ cartItem, loadCart }) {
+  const [quantity, setQuantity] = useState(cartItem.quantity)
+
+  const updateCartItem = async () => {
+    await axios.put(`/api/cart-items/${cartItem.productId}`, { quantity })
+    await loadCart()
+  }
+
+  const deleteCartItem = async () => {
+    await axios.delete(`/api/cart-items/${cartItem.productId}`)
+    await loadCart()
+  }
+
   return (
     <>
-      <img className="product-image"
-        src={cartItem.product.image} />
+      <img className="product-image" src={cartItem.product.image} />
       <div className="cart-item-details">
-        <div className="product-name">
-          {cartItem.product.name}
-        </div>
+        <div className="product-name">{cartItem.product.name}</div>
         <div className="product-price">
           {formatMoney(cartItem.product.priceCents)}
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:
+            <input
+              type="number"
+              value={quantity}
+              min="1"
+              onChange={(e) => setQuantity(Number(e.target.value))}
+            />
           </span>
-          <span className="update-quantity-link link-primary">
+          <span className="update-quantity-link link-primary" onClick={updateCartItem}>
             Update
           </span>
-          <span className="delete-quantity-link link-primary">
+          <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
             Delete
           </span>
         </div>
@@ -27,4 +44,5 @@ function CartItemDetails({ cartItem }) {
     </>
   )
 }
+
 export default CartItemDetails
